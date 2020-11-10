6a9660c69f078c7c6d5fc0cb3980be8abf198503
Iosif <iatzamis@stackmasters.eu>
2020-11-10 15:02:19 +0200

    fix bug in redirect to sign in page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux';
+
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -34,6 +35,6 @@ const Header = ({ currentUser }) => (
 
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser
-})
+});
 
 export default connect(mapStateToProps)(Header);
