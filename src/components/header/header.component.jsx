a8efebce3e376ac2f9cd65b2ffdbc89187b4cbb3
Iosif <iatzamis@stackmasters.eu>
2020-11-10 16:44:41 +0200

    cart toggle action

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,8 +7,9 @@ import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
-
-const Header = ({ currentUser }) => (
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -25,16 +26,20 @@ const Header = ({ currentUser }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
-      )}
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )}
+      <CartIcon />
     </div>
+    {
+      hidden ? null : <CartDropdown/>
+    }
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser, hidden
 });
 
 export default connect(mapStateToProps)(Header);
