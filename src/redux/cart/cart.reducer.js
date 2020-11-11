9866b27b03e3a9e161da917042db2a87a7d7bd32
Iosif <iatzamis@stackmasters.eu>
2020-11-11 12:12:50 +0200

    addItem to cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,9 @@
-import {CartActionTypes} from './cart.types'
+import {
+    CartActionTypes
+} from './cart.types'
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,7 +13,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 hidden: !state.hidden
             };
-
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload]
+            }
             default:
                 return state;
     }
