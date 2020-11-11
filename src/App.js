d33c0fe8d353127751d943721c7016e05f663952
Iosif <iatzamis@stackmasters.eu>
2020-11-11 16:22:25 +0200

    created checkout page

--- src/App.js
+++ src/App.js
@@ -8,9 +8,19 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import CheckoutPage from './pages/checkout/checkout.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector'
+
+
+/*
+          test credit card for payments :
+          4242 4242 4242 4242  - Exp 01/20 - CVV:123 
+ */
+
+
+
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -44,6 +54,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+
           <Route
             exact
             path='/signin'
