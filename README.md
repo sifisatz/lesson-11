7b655029ee67d99ff14030d65a3fa8ba49271eaa
Iosif <iatzamis@stackmasters.eu>
2020-11-21 14:26:38 +0200

    create root saga middleware

--- README.md
+++ README.md
@@ -183,3 +183,5 @@ console.log(gen.next().value);
 **redux-saga** is a library that aims to make application side effects (i.e. asynchronous things like data fetching and impure things like accessing the browser cache) easier to manage, more efficient to execute, easy to test, and better at handling failures.
 
 https://redux-saga.js.org/ 
+
+take, takeLatest, takeEvery, delay, put, take
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+
+
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component {
   componentDidMount() {
-    const { fetchCollectionsStartAsync } = this.props;
-    fetchCollectionsStartAsync()
+    const { fetchCollectionsStart } = this.props;
+
+    fetchCollectionsStart();
   }
+
   render() {
-    const { match } = this.props
+    const { match } = this.props;
 
     return (
       <div className='shop-page'>
@@ -26,13 +31,14 @@ class ShopPage extends React.Component {
         />
       </div>
     );
-
   }
 }
 
-
-
 const mapDispatchToProps = dispatch => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
-})
-export default connect(null, mapDispatchToProps)(ShopPage);
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopPage);
